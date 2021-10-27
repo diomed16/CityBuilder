984bf5b2715e6676aa27d1e22e7c27511068fe21
diomed16 <38938619+diomed16@users.noreply.github.com>
2021-10-27 20:33:33 +0600

    Update game_state_start.cpp

--- game_state_start.cpp
+++ game_state_start.cpp
@@ -18,3 +18,24 @@ void GameStateStart::update(const float dt)
 {
 
 }
+void GameStateStart::handleInput(const float dt)
+{
+    sf::Event event;
+    while(this->window.pollEvent(event))
+    {
+    switch(event.type)
+    {
+        case sf:Event::Closed:
+            {
+                this->window.close();
+                break;
+            }
+        case sf::Event::Resized:
+            {
+                this->view.setSize(event.size.width, event.size.height);
+                this->game->background.setPosition(this->game->window.mapPixelToCoodrs(sf::Vector2i(0,0));
+                this->
+            }
+    }
+        
+}
